d2ee291d6baafd5a351f7ba41c7e5de497cdb8c7
Dmytro Melnyk <dmelnyk0@gmail.com>
2025-10-15 12:55:01 +0000

    db models init

---
+++ app/database/models/__init__.py
@@ -0,0 +1,15 @@
+"""
+Database Models Package
+"""
+
+from .user import User
+from .profile import UserProfile
+from .subscription import Subscription
+from .generation import Generation
+
+__all__ = [
+    'User',
+    'UserProfile',
+    'Subscription',
+    'Generation'
+]
