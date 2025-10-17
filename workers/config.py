34f2c79e6464ebf38bf4dac8811df40ff24975b2
Dmytro Melnyk <dmelnyk0@gmail.com>
2025-10-17 13:27:42 +0000

    main.py

--- workers/config.py
+++
@@ -1,8 +0,0 @@
-# workers/config.py
-from app.config.settings import settings
-
-WORKER_SETTINGS = {
-    'database_url': settings.MONGODB_URL,
-    'rabbitmq_url': settings.RABBITMQ_URL,
-    'ai_provider': settings.ACTIVE_AI_PROVIDER,
-}
